d91810703e12ba506665078c24b0abad7ca6b47c
kalovee <kalovee@users.noreply.github.com>
2026-05-22 11:26:44 +0800

    Make quiz options more misleading

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ei-graduation-quiz-v1";
+const CACHE_NAME = "ei-graduation-quiz-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
